e1b4c072f4aed3004e2bf385889dc3648acae3e3
Guilherme Alexandrino <guiialexandrino@gmail.com>
2026-03-11 00:09:35 -0300

    fix: request error 429 caused by promise all

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,14 @@
 </template>
 
 <script setup lang="ts">
-import type { Card } from './types/Cards';
-import { QUERY_PAGES, QUERY_CHARACTERS, connectApi } from './utils/connectApi';
-import { computed, onBeforeMount, onMounted, ref } from 'vue';
-import { RouterView, useRoute } from 'vue-router';
-import { useStore } from 'vuex';
-import { useQuasar } from 'quasar';
-import Header from './components/Header/Header.vue';
-import Loading from './components/Loading/Loading.vue';
+import type { Card } from "./types/Cards";
+import { QUERY_PAGES, QUERY_CHARACTERS, connectApi } from "./utils/connectApi";
+import { computed, onBeforeMount, onMounted, ref } from "vue";
+import { RouterView, useRoute } from "vue-router";
+import { useStore } from "vuex";
+import { useQuasar } from "quasar";
+import Header from "./components/Header/Header.vue";
+import Loading from "./components/Loading/Loading.vue";
 
 const store = useStore();
 const route = useRoute();
@@ -35,54 +35,45 @@ const isLoading = computed(() => {
 const loadingDone = ref(false);
 
 onBeforeMount(() => {
-  const data: any = $q.sessionStorage.getItem('chars');
+  const data: any = $q.sessionStorage.getItem("chars");
   if (data) {
-    store.dispatch('updateChars', [...data]);
-    store.dispatch('updateCharsBackup', [...data]);
+    store.dispatch("updateChars", [...data]);
+    store.dispatch("updateCharsBackup", [...data]);
   }
 });
 
 onMounted(async () => {
-  const data: any = $q.sessionStorage.getItem('chars');
+  const data: any = $q.sessionStorage.getItem("chars");
   if (!data) await init();
   else loadingDone.value = true;
 });
 
 async function init() {
-  store.dispatch('loadingInit');
-  connectApi(QUERY_PAGES).then(async (result) => {
-    let pages = 0;
-    pages = result.data.characters.info.pages;
-    const charactersPromises = [];
-    const charactersCard: Card[] = [];
-    let loadingValueProcess = 0;
+  store.dispatch("loadingInit");
+  const result = await connectApi(QUERY_PAGES);
+  let pages = 0;
+  pages = result.data.characters.info.pages;
+  const charactersCard: Card[] = [];
+  let loadingValueProcess = 0;
 
-    for (let i = 1; i <= pages; i++) {
-      charactersPromises.push(
-        connectApi(QUERY_CHARACTERS(i), () => {
-          loadingValueProcess = loadingValueProcess + 1;
+  for (let i = 1; i <= pages; i++) {
+    const characters = await connectApi(QUERY_CHARACTERS(i));
 
-          const updateLoading = Math.round((loadingValueProcess / pages) * 100);
-
-          store.dispatch('updateLoadingProcess', updateLoading);
-        })
-      );
-    }
-
-    await Promise.all(charactersPromises).then((characters) => {
-      characters.forEach((array) => {
-        array.data.characters.results.forEach((item: Card) => {
-          charactersCard.push(item);
-        });
-      });
+    characters.data.characters.results.forEach((item: Card) => {
+      charactersCard.push(item);
     });
 
-    $q.sessionStorage.set('chars', [...charactersCard]);
-    store.dispatch('updateChars', [...charactersCard]);
-    store.dispatch('updateCharsBackup', [...charactersCard]);
-    store.dispatch('loadingDone');
-    store.dispatch('updateLoadingProcess', 0);
-    loadingDone.value = true;
-  });
+    loadingValueProcess = loadingValueProcess + 1;
+
+    const updateLoading = Math.round((loadingValueProcess / pages) * 100);
+    store.dispatch("updateLoadingProcess", updateLoading);
+  }
+
+  $q.sessionStorage.set("chars", [...charactersCard]);
+  store.dispatch("updateChars", [...charactersCard]);
+  store.dispatch("updateCharsBackup", [...charactersCard]);
+  store.dispatch("loadingDone");
+  store.dispatch("updateLoadingProcess", 0);
+  loadingDone.value = true;
 }
 </script>
